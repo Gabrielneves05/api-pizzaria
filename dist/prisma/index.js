8f061c43d8a970b9be16ba818be0cdda7f38b283
Gabrielneves05 <gabrielneves05@outlook.com>
2025-04-16 23:35:18 -0300

    wip

--- dist/prisma/index.js
+++ dist/prisma/index.js
@@ -1,5 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
-const prismaClient = new client_1.PrismaClient();
+// A Vercel cria novas instâncias serverless, então a conexão deve ser reutilizada.
+const prismaClient = global.prisma || new client_1.PrismaClient();
+if (process.env.NODE_ENV !== 'production') {
+    global.prisma = prismaClient; // Para desenvolvimento, reutiliza a conexão entre requisições
+}
 exports.default = prismaClient;
--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,5 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
-const prismaClient = new PrismaClient()
+// A Vercel cria novas instâncias serverless, então a conexão deve ser reutilizada.
+const prismaClient = global.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') {
+  global.prisma = prismaClient; // Para desenvolvimento, reutiliza a conexão entre requisições
+}
 
 export default prismaClient;
